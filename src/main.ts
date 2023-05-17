c3992fa54e7ecd70391e2d34daf1abf43c69b136
Vadym Maslovskyi <vmaslovskyi@perpet.io>
2023-05-18 00:04:48 +0300

    config cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as cors from "cors"
 
 async function start() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 4000
 
   app.use(cookieParser())
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }))
 
   app.setGlobalPrefix("api")
   await app.listen(PORT, () => console.log(`Server starts at http://localhost:${PORT}`));
